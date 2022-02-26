fac22558d6c2bfbc08d82c6e746fb5b8d05f8176
Luke Taylor <luke@dibza.co.uk>
2022-02-26 15:21:14 +0000

    remove player control on pause

--- Assets/Scripts/PlayerPaddle.cs
+++ Assets/Scripts/PlayerPaddle.cs
@@ -7,6 +7,11 @@ public class PlayerPaddle : Paddle
     // Update is called once per frame
     void Update()
     {
+        if (GameManager.instance.isPaused)
+        {
+            UpdateMotor(0);
+            return;
+        }
         UpdateMotor(Input.GetAxisRaw(isPlayer1 ? "Vertical" : "Vertical2"));
     }
 }
